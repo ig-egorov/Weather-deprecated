7b84ae1dac436295325080528fb6993b2755cecf
ig-egorov <igpapegorov@gmail.com>
2021-05-04 13:17:01 +0300

    added necessary packages

--- app/src/main/java/com/example/weather/weather_details/WeatherDetailsFragment.kt
+++ app/src/main/java/com/example/weather/weather_details/WeatherDetailsFragment.kt
@@ -6,15 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.weather.R
+import com.example.weather.databinding.FragmentWeatherDetailsBinding
 
 class WeatherDetailsFragment : Fragment() {
+
+    private lateinit var binding: FragmentWeatherDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather_details, container, false)
+        binding = FragmentWeatherDetailsBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
 }
---
+++ app/src/main/java/com/example/weather/weather_details/WeatherDetailsViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.weather.weather_details
+
+import androidx.lifecycle.ViewModel
+
+class WeatherDetailsViewModel : ViewModel() {
+
+}
